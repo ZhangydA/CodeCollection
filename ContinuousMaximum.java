0b65a558a98ecac5209eb12ef14d5b7b73861514
ZhangydA <37284219+ZhangydA@users.noreply.github.com>
2018-03-27 22:32:16 +0800

    Create ContinuousMaximum.java

---
+++ ContinuousMaximum.java
@@ -0,0 +1,19 @@
+public class ContinuousMaximum {
+	public int continuousMaxinum(int arr[]){
+		int counter = 0;
+		for (int i = 0;i < arr.length;i ++){
+			if (arr[i] < 0) counter ++;
+		}
+		//如果所有的值都为负值，返回0
+		if (counter == arr.length) return 0;
+		int sum = 0,ans = Integer.MIN_VALUE,mins = 0;
+		for (int i = 0;i < arr.length;i ++){
+			sum += arr[i];
+			if (sum - mins > ans)
+				ans = sum - mins;
+			if (mins > sum)
+				mins = sum;
+		}
+		return ans;
+	}
+}
